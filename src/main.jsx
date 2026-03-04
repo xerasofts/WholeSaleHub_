4b8908af383a2d011bd9b9db609ff7b5b31ee6e1
akhilpa555 <46228116+akhilpa555@users.noreply.github.com>
2026-03-04 12:39:45 +0530

    final

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,10 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+const basename = import.meta.env.PROD ? '/WholeSaleHub_' : '/'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename={basename}>
     <App />
   </BrowserRouter>
 )
